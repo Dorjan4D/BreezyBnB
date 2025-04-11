cf1cb33d73a8d47dcf563c45568b73af6aad1bb6
Dorjan Štrbac <dorjan.strbac@fer.hr>
2025-04-11 13:38:23 +0200

    Initial commit with Spring Boot project using latest versions

---
+++ src/main/java/com/breezybnb/BreezyBnBApplication.java
@@ -0,0 +1,13 @@
+package com.breezybnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreezyBnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreezyBnBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breezybnb/BreezyBnBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breezybnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreezyBnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
